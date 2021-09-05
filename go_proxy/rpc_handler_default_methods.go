5c015a75ed736fd8353cb8f2275ae730b2a76aae
Oleg Andriyanov <o.andriyanov@gmail.com>
2021-09-06 00:20:55 +0300

    More default methods

--- go_proxy/rpc_handler_default_methods.go
+++ go_proxy/rpc_handler_default_methods.go
@@ -19,3 +19,55 @@ func (h *RPCHandler) AttachedToExistingProcess(req map[string]interface{}, resp 
 func (h *RPCHandler) Detach(req map[string]interface{}, resp *map[string]interface{}) error {
 	return h.defaultHandler(fqmn("Detach"), req, resp)
 }
+
+func (h *RPCHandler) Recorded(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("Recorded"), req, resp)
+}
+
+func (h *RPCHandler) Command(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("Command"), req, resp)
+}
+
+func (h *RPCHandler) FindLocation(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("FindLocation"), req, resp)
+}
+
+func (h *RPCHandler) CreateBreakpoint(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("CreateBreakpoint"), req, resp)
+}
+
+func (h *RPCHandler) LastModified(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("LastModified"), req, resp)
+}
+
+func (h *RPCHandler) Stacktrace(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("Stacktrace"), req, resp)
+}
+
+func (h *RPCHandler) ProcessPid(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("Restart"), req, resp)
+}
+
+func (h *RPCHandler) Restart(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("Restart"), req, resp)
+}
+
+func (h *RPCHandler) Eval(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("Eval"), req, resp)
+}
+
+func (h *RPCHandler) GetBreakpoint(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("GetBreakpoint"), req, resp)
+}
+
+func (h *RPCHandler) AmendBreakpoint(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("AmendBreakpoint"), req, resp)
+}
+
+func (h *RPCHandler) ListBreakpoints(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("ListBreakpoints"), req, resp)
+}
+
+func (h *RPCHandler) ListGoroutines(req map[string]interface{}, resp *map[string]interface{}) error {
+	return h.defaultHandler(fqmn("ListGoroutines"), req, resp)
+}
