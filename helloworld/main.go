1c14854a36713573a241888c651a3ceb1c894fd9
Oleg Andriyanov <o.andriyanov@gmail.com>
2021-12-18 20:47:29 +0300

    Add more lines into the helloworld

--- helloworld/main.go
+++ helloworld/main.go
@@ -9,4 +9,7 @@ func main() {
 	fmt.Println("Hello, world!")
 	fmt.Printf("%s\n", os.Args[0])
 	fmt.Println("Good bye, cruel world!")
+	fmt.Println("Good bye, cruel world!1")
+	fmt.Println("Good bye, cruel world!2")
+	fmt.Println("Good bye, cruel world!3")
 }
