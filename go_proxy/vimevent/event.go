8d1eefaee683c09c610e5adf81a74045f5359895
Oleg Andriyanov <o.andriyanov@gmail.com>
2021-12-25 20:14:23 +0300

    Style

--- go_proxy/vimevent/event.go
+++ go_proxy/vimevent/event.go
@@ -8,14 +8,6 @@ type BreakpointsUpdated struct{}
 type CommandIssued struct{}
 type StateUpdated struct{}
 
-func (*BreakpointsUpdated) Kind() string {
-	return "BREAKPOINTS_UPDATED"
-}
-
-func (*CommandIssued) Kind() string {
-	return "COMMAND_ISSUED"
-}
-
-func (*StateUpdated) Kind() string {
-	return "STATE_UPDATED"
-}
+func (*BreakpointsUpdated) Kind() string { return "BREAKPOINTS_UPDATED" }
+func (*CommandIssued) Kind() string      { return "COMMAND_ISSUED" }
+func (*StateUpdated) Kind() string       { return "STATE_UPDATED" }
