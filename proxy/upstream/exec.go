234252b8b10f11da193b8de22ecf8dda1b67df88
Oleg Andriyanov <o.andriyanov@gmail.com>
2022-09-20 18:14:56 +0300

    Allow specifying command line arguments for the exec command

--- proxy/upstream/exec.go
+++ proxy/upstream/exec.go
@@ -6,22 +6,20 @@ import (
 
 func init() {
 	AddStartOptionParser("exec", func(argv []string) (StartOption, error) {
-		if len(argv) != 1 {
-			return nil, xerrors.Errorf("exactly one argument expected")
+		if len(argv) < 1 {
+			return nil, xerrors.Errorf("at least one argument expected")
 		}
-		return &Exec{ExecutablePath: argv[0]}, nil
+		return &Exec{ExecutablePath: argv[0], CmdLineArgs: argv[1:]}, nil
 	})
 }
 
 type Exec struct {
 	ExecutablePath string
+	CmdLineArgs    []string
 }
 
 func (d *Exec) isStartOption() {}
 
 func (d *Exec) Argv() []string {
-	return []string{
-		"exec",
-		d.ExecutablePath,
-	}
+	return append([]string{"exec", d.ExecutablePath}, d.CmdLineArgs...)
 }
